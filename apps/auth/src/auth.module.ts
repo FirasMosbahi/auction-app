1c13656c99bdb21533428226897892f6bebcc27d
firas <firas.mosbehi@insat.ucar.tn>
2023-06-06 15:28:21 +0100

    auth microservice implemented

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/auction')],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/auction'),
+    JwtModule.register({
+      secret: 'lovester-auction-app-secret-key',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
