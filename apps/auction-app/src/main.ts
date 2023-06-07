17d74f76a602fce817af0fe324f5c4da80c06038
firas <firas.mosbehi@insat.ucar.tn>
2023-06-07 17:02:57 -0400

    documentation implemented

--- apps/auction-app/src/main.ts
+++ apps/auction-app/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(3000);
 }
